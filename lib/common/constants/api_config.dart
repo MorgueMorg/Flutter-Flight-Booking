20f87c90271dcd06f7563fd02865574cca30267a
Aktan Dzhanaliev <morg@MacBook-Pro-Aktan.local>
2023-09-04 21:24:43 +0600

    Updated api key (more requests) :cyclone:

--- lib/common/constants/api_config.dart
+++ lib/common/constants/api_config.dart
@@ -1,4 +1,8 @@
+// У апишки часто приходят в поля "null", поэтому я юзаю только один эндпоинт(самый надежный)
 abstract class ApiConfig {
-  // У апишки часто приходят в поля "null", поэтому я юзаю только один эндпоинт(самый надежный) 
-  static String apiUrl = "http://api.aviationstack.com/v1/flights?access_key=0c6f621f57c5d3e34d78da2744c294de&limit=50";
+  // Старая апишка (330 свободных запросов)
+  // static String apiUrl = "http://api.aviationstack.com/v1/flights?access_key=0c6f621f57c5d3e34d78da2744c294de&limit=50";
+
+  static String apiUrl =
+      "http://api.aviationstack.com/v1/flights?access_key=1d729e31d4960efce8c82aab88b854e7&limit=50";
 }
